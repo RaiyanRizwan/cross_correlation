f2e2dbfd86ebadebc7c591dd576b579da433b27b
Raiyan Rizwan <114321382+RaiyanRizwan@users.noreply.github.com>
2022-12-02 22:31:31 -0800

    Update cross_corr.py

--- cross_corr.py
+++ cross_corr.py
@@ -1,7 +1,4 @@
-signal_1 = [1, 1, -1, 1, 1]
-signal_2 = [1, 1, 1, -1, -1]
-received = [.2, .2, 1, 1, -1.2, 1, 1, .2, -.2]
-signal_list = [signal_1, signal_2]
+import math
 
 def zero_pad(l1, k):
 	return l1 + [0]*(k-len(l1))
@@ -9,13 +6,33 @@ def zero_pad(l1, k):
 def shift(l1, k):
 	return l1[-k:] + l1[:-k]
 
-def compute_corr(signal, recieved):
+def normalized(signal):
+	n = norm(signal)
+	return list(map(lambda e: e/n, signal))
+
+def norm(signal):
+	return math.sqrt(inner_product(signal, signal))
+
+def compute_corr(signal, received):
 	k = len(received)
 	signal = zero_pad(signal, k)
 	corrs = []
 	for s in range(k):
-		corrs.append(round(sum(map(lambda pair: pair[0]*pair[1], zip(shift(signal, s), received))), 2))
-	return max(corrs)
+		signal_shifted = shift(signal, s)
+		corrs.append(round(inner_product(signal_shifted, received), 2))
+	print(corrs)
+	highest_corr = max(corrs)
+	return (highest_corr, corrs.index(highest_corr))	
 
-def correlations(signal_list, recieved):
-	return {f'signal_{i}': compute_corr(signal_list[i], recieved) for i in range(len(signal_list))}
+def window_cross_corr(signal, received):
+	k = len(received)
+	corrs = []
+	for s in range(k):
+		corrs.append(inner_product(normalized(signal), normalized(shift(received, -k)[0:len(signal)])))
+	return (max(corrs), corrs.index(max(corrs)))
+
+def inner_product(s1, s2):
+	return sum(map(lambda p: p[0]*p[1], zip(s1,s2)))
+
+def correlations(signal_list, received):
+	return {f'signal_{i}': window_cross_corr(signal_list[i], received) for i in range(len(signal_list))}
